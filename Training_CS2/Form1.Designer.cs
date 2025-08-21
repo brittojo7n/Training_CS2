04869acffe900a381afaae5db38b4298f983b99f
brittojo7n <sjbritto2003@mail.com>
2025-08-21 14:36:17 +0530

    commit

--- Training_CS2/Form1.Designer.cs
+++ Training_CS2/Form1.Designer.cs
@@ -29,41 +29,63 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.usernameLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
-            this.verifyButton = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // usernameLabel
+            // label1
             // 
-            this.usernameLabel.AutoSize = true;
-            this.usernameLabel.BackColor = System.Drawing.Color.Snow;
-            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
-            this.usernameLabel.Location = new System.Drawing.Point(66, 107);
-            this.usernameLabel.Name = "usernameLabel";
-            this.usernameLabel.Size = new System.Drawing.Size(92, 22);
-            this.usernameLabel.TabIndex = 0;
-            this.usernameLabel.Text = "Username";
-            this.usernameLabel.Click += new System.EventHandler(this.label1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label1.Location = new System.Drawing.Point(128, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label2.Location = new System.Drawing.Point(131, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
             // 
             // usernameTextBox
             // 
-            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
-            this.usernameTextBox.Location = new System.Drawing.Point(166, 105);
+            this.usernameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.usernameTextBox.Location = new System.Drawing.Point(236, 134);
             this.usernameTextBox.Name = "usernameTextBox";
-            this.usernameTextBox.Size = new System.Drawing.Size(183, 28);
-            this.usernameTextBox.TabIndex = 1;
+            this.usernameTextBox.Size = new System.Drawing.Size(183, 26);
+            this.usernameTextBox.TabIndex = 2;
             this.usernameTextBox.TextChanged += new System.EventHandler(this.usernameTextBox_TextChanged);
             // 
-            // verifyButton
+            // passwordTextBox
             // 
-            this.verifyButton.Location = new System.Drawing.Point(262, 150);
-            this.verifyButton.Name = "verifyButton";
-            this.verifyButton.Size = new System.Drawing.Size(85, 28);
-            this.verifyButton.TabIndex = 2;
-            this.verifyButton.Text = "Verify";
-            this.verifyButton.UseVisualStyleBackColor = true;
-            this.verifyButton.Click += new System.EventHandler(this.verifyButton_Click_1);
+            this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.passwordTextBox.Location = new System.Drawing.Point(236, 178);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(183, 26);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.loginButton.Location = new System.Drawing.Point(206, 236);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(85, 28);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // Form1
             // 
@@ -72,11 +94,13 @@
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1038, 516);
-            this.Controls.Add(this.verifyButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.usernameTextBox);
-            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "t";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -85,9 +109,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox usernameTextBox;
-        private System.Windows.Forms.Button verifyButton;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
     }
 }
 
--- Training_CS2/Form1.cs
+++ Training_CS2/Form1.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Training_CS2
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> userCredentials = new Dictionary<string, string>
+        {
+            { "dave", "2345" },
+            { "sam", "1234" }
+        };
+
         public Form1()
         {
             InitializeComponent();
@@ -15,10 +22,9 @@ namespace Training_CS2
 
         }
 
-        private void verifyButton_Click_1(object sender, EventArgs e)
+        private void passwordTextBox_TextChanged(object sender, EventArgs e)
         {
-            string username = usernameTextBox.Text;
-            MessageBox.Show(username + " verified");
+
         }
 
         private void usernameTextBox_TextChanged(object sender, EventArgs e)
@@ -29,6 +35,34 @@ namespace Training_CS2
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string username = usernameTextBox.Text;
+                string password = passwordTextBox.Text;
+
+                if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+                {
+                    MessageBox.Show("Username and password cannot be empty.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (userCredentials.ContainsKey(username) && userCredentials[username] == password)
+                {
+                    MessageBox.Show($"Welcome, {username}! You are verified.", "Login Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unexpected error occurred: " + ex.Message, "System Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
     }
 }
